7ad466e1f1cbfaded444c5d0bc3f0f84d382fc92
hws2002 <hwooseok123@gmail.com>
2024-03-28 11:50:46 +0000

    update : storage.py

--- Crawler/utils/storage.py
+++ Crawler/utils/storage.py
@@ -5,11 +5,13 @@ def store_outfits(outfits):
     # Define the field names for the CSV file
     fieldnames = ['상품번호', '코디 페이지 링크', '코디 사진 링크', '업로드 날짜', '코디 이름', '조회수', '댓글수', '성별']
     
-    # Define the output file path
-    output_file = os.path.join('..', 'data', 'output.csv')
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Define the output file path relative to the script directory
+    output_file = os.path.join(script_dir, '..', 'data', 'outfits.csv')
     
     # Open the CSV file in write mode
-    with open('outfits.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
         # Create a CSV writer object
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
